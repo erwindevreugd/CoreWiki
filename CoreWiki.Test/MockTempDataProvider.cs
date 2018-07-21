7e913ee9e35d2f2f94a0e48df3a3ba46244bf2ca
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-07-21 16:15:35 +0200

    Added startup tests

---
+++ CoreWiki.Test/MockTempDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+
+namespace CoreWiki.Test
+{
+    public class MockTempDataProvider : ITempDataProvider
+    {
+        public IDictionary<string, object> LoadTempData(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTempData(HttpContext context, IDictionary<string, object> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CoreWiki.Test/Notifications/StartupExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using CoreWiki.Core.Notifications;
+using CoreWiki.Notifications;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CoreWiki.Test.Notifications
+{
+    public class StartupExtensionsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+            serviceCollection.AddSingleton<ITempDataProvider>(new MockTempDataProvider());
+            serviceCollection.AddSingleton<IRazorViewEngine>(new MockRazorViewEngine());
+
+            serviceCollection.AddEmailNotifications();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            Assert.NotNull(serviceProvider.GetService<IEmailMessageFormatter>());
+            Assert.NotNull(serviceProvider.GetService<IEmailNotifier>());
+            Assert.NotNull(serviceProvider.GetService<INotificationService>());
+            Assert.NotNull(serviceProvider.GetService<ITemplateParser>());
+            Assert.NotNull(serviceProvider.GetService<ITemplateProvider>());
+        }
+    }
+}
